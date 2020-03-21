4e6a2c550e3c1799fc4189e4de63b224e2c402be
hyecheon <rainbow880616@gmail.com>
2020-03-21 18:16:10 +0900

    휴으...

--- ch01/decorator.go
+++ ch01/decorator.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+//ProcessClass struct
+type ProcessClass struct{}
+
+//ProcessClass method process
+func (process *ProcessClass) process() {
+	fmt.Println("ProcessClass process")
+}
+
+//ProcessDecorator struct
+type ProcessDecorator struct {
+	processInstance *ProcessClass
+}
+
+func (decorator *ProcessDecorator) process() {
+	if decorator.processInstance == nil {
+		fmt.Println("ProcessDecorator process")
+	} else {
+		fmt.Printf("ProcessDecorator process and ")
+		decorator.processInstance.process()
+	}
+}
+func main() {
+	process := &ProcessClass{}
+	decorator := &ProcessDecorator{}
+
+	decorator.process()
+	decorator.processInstance = process
+	decorator.process()
+}
---
+++ ch01/proxy.go
@@ -0,0 +1,42 @@
+//main package has examples shown
+// in Hands-On Data Structures and algorithms with Go book
+package main
+
+// importing fmt package
+import (
+	"fmt"
+)
+
+//IRealObject interface
+type IRealObject interface {
+	performAction()
+}
+
+//RealObject struct
+type RealObject struct{}
+
+//RealObject class implements IRealObject interface.The class has method performAction.
+//RealObject class method performAction
+func (realObject *RealObject) performAction() {
+	fmt.Println("RealObject performAction()")
+}
+
+//VirtualProxy struct
+type VirtualProxy struct {
+	realObject *RealObject
+}
+
+//VirtualProxy class method performAction
+func (virtualProxy *VirtualProxy) performAction() {
+	if virtualProxy.realObject == nil {
+		virtualProxy.realObject = &RealObject{}
+	}
+	fmt.Println("Virtual Proxy performAction()")
+	virtualProxy.realObject.performAction()
+}
+
+// main method
+func main() {
+	var object VirtualProxy = VirtualProxy{}
+	object.performAction()
+}
